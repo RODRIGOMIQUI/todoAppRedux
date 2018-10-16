36a6b1ce07606b4ef9fe40ab0f40878165007bdc
Rodrigo Miqui <rmiqui@gmail.com>
2018-10-16 20:38:30 -0300

    App para controle de tarefas

---
+++ frontEnd/src/about/about.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa obcaecati error, labore molestiae voluptatibus eos qui iusto iure a eum dolorem dignissimos pariatur iste consectetur consequuntur! Ratione omnis cum voluptas.</p>
+
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, porro ipsum illo voluptates ipsa culpa corrupti fugiat tenetur id vero optio est tempore? Totam debitis laboriosam cupiditate, fuga autem culpa?</p>
+
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, consequatur. Eum impedit rerum odit voluptates itaque mollitia, omnis natus, ad voluptate ipsum a dolor suscipit ea expedita deserunt dignissimos quos!</p>        
+    </div>
+)
